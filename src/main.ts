2c2f945e8f91b8dc4ccd39f68074f8a84174113a
xanddybr <xanddybr@gmail.com>
2026-04-29 06:04:54 -0300

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import './style.css';
 
 const API_BASE_URL = window.location.hostname === 'localhost'
     ? 'http://localhost:8080'
-    : 'https://beta.misturadeluz.com';
+    : 'https://beta.misturadeluz.com/beta';
 
 const APP_VERSION = "1.0.0beta";
 
